e2266a6fa90bed423f95a976c31ed4d0f133bb0b
Jasper Alblas <just_jasper@hotmail.com>
2024-09-21 20:28:53 +0200

    Current player is now tracked

--- src/Player.js
+++ src/Player.js
@@ -3,6 +3,19 @@ class Player {
     this.type = type;
     this.board = board;
   }
+
+  // Static property to hold the current player
+  static currentPlayer = null;
+
+  // Static method to set the current player
+  static setCurrentPlayer(player) {
+    Player.currentPlayer = player;
+  }
+
+  // Static method to get the current player
+  static getCurrentPlayer() {
+    return Player.currentPlayer;
+  }
 }
 
 module.exports = Player;
